bf4408137de90e438b19ce553d8fc6872efedfe7
Yaatu Adem <yaatu.adem@gmail.com>
2025-04-25 17:08:50 -0400

    Update puppeteer.config.cjs
    Updated for local + Render use

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -2,4 +2,7 @@ const { join } = require('path');
 /**
 * @type {import("puppeteer").Configuration}
 */
-module.exports = {cacheDirectory: join(__dirname, '.cache', 'puppeteer')};
+
+if (process.env.RENDER === "true") {
+    module.exports = {cacheDirectory: join(__dirname, '.cache', 'puppeteer')};
+}
